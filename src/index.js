bd70bcfc8c56f4fd51edd2ae79ca0a53401d23a4
Anton Andrushin <andrushin.anton@gmail.com>
2020-01-04 00:01:06 +0200

    add react router dom

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ReactRouter } from 'react-router-dom';  
+
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <App /> 
+    </BrowserRouter>,
+    document.getElementById('root')
+);
