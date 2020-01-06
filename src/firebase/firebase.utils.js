8c82e5edadc03c9a9cd4cfa4edc8ccb12d210b6a
Anton Andrushin <andrushin.anton@gmail.com>
2020-01-06 15:13:05 +0200

    implemented firebase utils, including signup with user email and password

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,31 @@ const config = {
     measurementId: "G-FWBDQKLQ5Z"
   };
 
+  export const createUserProfileDocument = async (userAuth, additional) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additional
+        })
+      } catch(error) {
+        console.log('error creating user', error.message);
+      }
+    }
+
+    return userRef;
+  }
+
   firebase.initializeApp(config);
 
   export const auth = firebase.auth();
