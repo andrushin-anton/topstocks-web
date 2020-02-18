f1503840c8df2b2c0b64a26c7559fa132130ef06
Anton Andrushin <andrushin.anton@gmail.com>
2020-02-18 08:07:01 +0200

    updated app to use hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './App.css';
@@ -14,33 +14,24 @@ import { selectCurrentUser } from './redux/user/user.selectors';
 import CollectionPage from './pages/collection/collection.component';
 import { checkUserSession } from './redux/user/user.actions';
 
-class App extends Component {
+const App = ({ currentUser, checkUserSession }) => {
+  useEffect(() => {
+    checkUserSession()
+  }, [checkUserSession]);
 
-  unsubscribeFromAuth = null;
-
-  componentDidMount() {
-    const { checkUserSession } = this.props;
-    checkUserSession();
-  }
-
-  componentWillUnmount() {
-    this.unsubscribeFromAuth();
-  }
-
-  render() {
-    return (
-      <div>
-      <Header/>
-      <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route exact path='/shop' component={ShopPage} />
-          <Route path='/shop/:collectionId' component={CollectionPage} />
-          <Route exact path='/checkout' component={CheckoutPage} />
-          <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)} />
-      </Switch>
-      </div>
-    );
-  }
+  return (
+    <div>
+    <Header/>
+    <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+        <Route path='/shop/:collectionId' component={CollectionPage} />
+        <Route exact path='/checkout' component={CheckoutPage} />
+        <Route exact path='/signin' render={() => currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)} />
+    </Switch>
+    </div>
+  );
+  
 }
 
 const mapStateToProps = createStructuredSelector({
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import FormInput from '../../components/form-input/form-input.component';
 import CustomButton from '../../components/custom-button/custom-button.component';
 import { googleSignInStart, emailSignInStart } from '../../redux/user/user.actions';
@@ -6,67 +6,56 @@ import { googleSignInStart, emailSignInStart } from '../../redux/user/user.actio
 import './sign-in.styles.scss';
 import { connect } from 'react-redux';
 
-class SignIn extends Component {
-    constructor(props) {
-        super(props);
-        
-        this.state = {
-            email: '',
-            password: ''
-        }
-    }
+const SignIn = ({ emailSignInStart, googleSignInStart }) => {
+    const [userCredentials, setCredentials] = useState({ email: '', password: '' })
+    const { email, password } = userCredentials;
 
-    handleSubmit = async event => {
-        event.preventDefault();
-        const { emailSignInStart } = this.props;
-        const { email, password } = this.state;
-        
+    const handleSubmit = async event => {
+        event.preventDefault();       
         emailSignInStart(email, password);
     }
 
-    handleChange = event => {
+    const handleChange = event => {
         const { value, name } = event.target;
-        this.setState({ [name]: value });
+        setCredentials({...userCredentials, [name]: value });
     }
     
-    render() {
-        const { googleSignInStart } = this.props;
-        return (
-            <div className='sign-in'>
-                <h2>I already have an account</h2>
-                <span>Sign In with email and password</span>
+    return (
+        <div className='sign-in'>
+            <h2>I already have an account</h2>
+            <span>Sign In with email and password</span>
 
-                <form onSubmit={this.handleSubmit}>
-                    <FormInput 
-                        name='email' 
-                        type='email' 
-                        value={this.state.email} 
-                        handleChange={this.handleChange}
-                        label='Email'
-                        required
-                    />
+            <form onSubmit={handleSubmit}>
+                <FormInput 
+                    name='email' 
+                    type='email' 
+                    value={email} 
+                    handleChange={handleChange}
+                    label='Email'
+                    required
+                />
 
-                    <FormInput 
-                        name='password' 
-                        type='password' 
-                        value={this.state.password} 
-                        handleChange={this.handleChange}
-                        label='Password'
-                        required
-                    />
+                <FormInput 
+                    name='password' 
+                    type='password' 
+                    value={password} 
+                    handleChange={handleChange}
+                    label='Password'
+                    required
+                />
 
-                    <div className='buttons'>
-                        <CustomButton type='submit'>Sign in</CustomButton>
-                        <CustomButton 
-                            type='button'
-                            onClick={googleSignInStart}
-                            isGoogleSignIn
-                        >{ ' ' }Sign in with Google{ ' ' }</CustomButton>
-                    </div>
-                </form>
-            </div>
-        );
-    }
+                <div className='buttons'>
+                    <CustomButton type='submit'>Sign in</CustomButton>
+                    <CustomButton 
+                        type='button'
+                        onClick={googleSignInStart}
+                        isGoogleSignIn
+                    >{ ' ' }Sign in with Google{ ' ' }</CustomButton>
+                </div>
+            </form>
+        </div>
+    );
+    
 }
 
 const mapDispatchToProps = dispatch => ({
