28c9c397da4d8ee00ab2c3383275979fd2d14996
Anton Andrushin <andrushin.anton@gmail.com>
2020-03-30 14:03:27 +0200

    add service worker and deploy to firebase hosting

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
+import * as serviceWorker from './serviceWorker.js';
 
 ReactDOM.render(
     <Provider store={store}>
@@ -18,3 +19,5 @@ ReactDOM.render(
     </Provider>,
     document.getElementById('root')
 );
+
+serviceWorker.register();
